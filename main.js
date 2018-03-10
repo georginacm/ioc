139390bd4efdbe862679207c326a8fde2e574853
georgina <georginacivico@gmail.com>
2018-03-10 12:42:36 +0100

    login refactor

--- main.js
+++ main.js
@@ -78,28 +78,5 @@ ipcMain.on('synchronous-message', (event, arg) => {
 
 ipcMain.on('invokeAction', function(event, data){
   console.info("login data:" + data.name +"  " + data.password);
-  returnUserToken(event, data);
+  loginUtils.login(event, data);
 })
-
-function returnUserToken(event, userData){
-  var userName=userData.name;
-  var pwd = userData.password;
-  pwd= crypto.createHash('md5').update(pwd).digest("hex");
-
-  var url="http://localhost:8080/eventfest/rest/users/connect";
-  url += "?login=" + userName;
-  url += "&pass=" + pwd;
-
-  var reqGet = http.request(url, function(res) {
-        console.log("statusCode: ", res.statusCode);
-          res.on('data', function(d) {
-              console.info('GET result:\n');
-              process.stdout.write(d);
-              event.sender.send('actionReply', d)
-          });
-      });
-  reqGet.end();
-  reqGet.on('error', function(e) {
-      console.error(e);
-  });
-}
--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,28 @@
-module.exports = {
-  login: function() {
-     let userName_home = document.getElementById("userName_home")
-     userName_home.innerHTML = "test"
+const crypto = require('crypto');
+const http = require('http');
 
+module.exports = {
+  login: function(event, userData) {
+    var userName=userData.name;
+    var pwd = userData.password;
+    pwd= crypto.createHash('md5').update(pwd).digest("hex");
+
+    var url="http://localhost:8080/eventfest/rest/users/connect";
+    url += "?login=" + userName;
+    url += "&pass=" + pwd;
+
+    var reqGet = http.request(url, function(res) {
+          console.log("statusCode: ", res.statusCode);
+            res.on('data', function(d) {
+                console.info('GET result:\n');
+                process.stdout.write(d);
+                event.sender.send('actionReply', d)
+            });
+        });
+    reqGet.end();
+    reqGet.on('error', function(e) {
+        console.error(e);
+    });
   },
 
   signup: function(){}
